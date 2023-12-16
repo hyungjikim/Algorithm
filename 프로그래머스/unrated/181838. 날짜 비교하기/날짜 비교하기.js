99b21f4aab4e5ccd88d00bb9f9ced896c374332d
Hyungji <84058944+hjk329@users.noreply.github.com>
2023-12-17 01:43:40 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.10 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,1 +1,1 @@
-const solution=(date1, date2)=> +(new Date(date1[0], date1[1] -1, date1[2]) < new Date(date2[0], date2[1] -1, date2[2]))
+const solution=(date1, date2)=>+(new Date(date1) < new Date(date2))
