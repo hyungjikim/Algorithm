e532d7fe1ff9b64ec7bc135a3fbd125b6029f327
Hyungji <84058944+hjk329@users.noreply.github.com>
2023-12-16 20:57:37 +0900

    [level unrated] Title: 카운트 다운, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.js
@@ -1,3 +1,3 @@
-function solution(start_num, end_num) {
-    return new Array(start_num - end_num +1).fill(1).map((_, idx) => idx + end_num).reverse()
+function solution(start, end) {
+    return new Array(start - end +1).fill(1).map((_, idx) => idx + end).reverse()
 }
