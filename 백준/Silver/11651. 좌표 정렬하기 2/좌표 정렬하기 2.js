a3e4a2f1fd93b5ebfeca909680f92189ca54d1bf
Hyungji <84058944+hjk329@users.noreply.github.com>
2024-03-09 17:19:47 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 476 ms, Memory: 60772 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,24 @@
+let fs = require('fs');
+
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let count = Number(input[0]);
+
+let arr = [];
+
+for(let i = 1; i <= count ; i++) {
+    arr.push(input[i].split(' ').map(Number));
+}
+
+arr.sort((a,b) => {
+    if(a[1] !== b[1]) return a[1] - b[1];
+    else return a[0] - b[0];
+})
+
+let result = ''
+
+for(let i = 0; i<arr.length; i++) {
+    result += arr[i].join(' ') + '\n'
+}
+
+console.log(result.trim());
