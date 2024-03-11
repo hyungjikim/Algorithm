7bd4f386e19987503b55e3373a489a50d785fed3
Hyungji <84058944+hjk329@users.noreply.github.com>
2024-03-11 22:46:13 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 9624 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -4,14 +4,12 @@ let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 let count = Number(input[0]);
 
-let arr = input[1].split(' ').map(Number);
-
-arr.sort((a,b) => a-b);
+let arr = input[1].split(' ').map(Number).sort((a,b) => a-b);
 
 let sum = 0;
 let result = 0;
 
-for(let i = 0; i< count; i++) {
+for(let i = 0; i < arr.length; i++) {
     sum += arr[i];
     result += sum;
 }
