8e0cb1c5355cd659eabbba3b7d4d947581f57b7f
Hyungji <84058944+hjk329@users.noreply.github.com>
2024-03-18 19:13:57 +0900

    [Silver II] Title: 예산, Time: 196 ms, Memory: 11876 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.js
+++ 백준/Silver/2512. 예산/예산.js
@@ -1,26 +1,28 @@
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let n = Number(input[0].split(' ')[0]); 
-let arr = input[1].split(' ').map(Number); 
-let m = Number(input[2]); 
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let start = 1; 
-let end = arr.reduce((a, b) => Math.max(a, b));
+let count = Number(input[0]);
+
+let arr = input[1].split(' ').map(Number);
+
+let m = Number(input[2]);
+
+let start = 1;
+let end = arr.reduce((a,c) => Math.max(a,c));
+
 let result = 0;
 
-while (start <= end) { 
+while(start <= end) {
     let mid = parseInt((start + end) / 2);
-    let total = 0; 
-    
-    for (x of arr) { 
-        total += Math.min(mid, x); 
+    let total = 0;
+    for(x of arr) {
+        total += Math.min(mid, x);
     }
-    
-    if (total <= m) { 
+    if(total <= m) {
         result = mid;
         start = mid + 1;
-    } else { 
+    } else {
         end = mid - 1;
     }
 }
